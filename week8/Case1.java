520ca444c2aa043c310dfd3a67e9f41e33fb6524
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 17:49:44 +0800

    Update and rename Soal1.java to Case1.java

--- week8/Case1.java
+++ week8/Case1.java
@@ -1,23 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package praktikum8;
+package week8;
 
 import java.util.*;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal1 {
-
+public class Case1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int length;
         int i = 0;
 
-        System.out.print("Masukan Jumlah Data = ");
+        System.out.print("Enter the Number of Data = ");
         length = input.nextInt();
 
         int[] array = new int[length];
@@ -26,50 +17,48 @@ public class Soal1 {
             System.out.println("Error!");
         } else {
             do {
-                System.out.print("Masukan Angka ke-" + (i + 1) + " = ");
+                System.out.print("Enter Number " + (i + 1) + " = ");
                 array[i] = input.nextInt();
                 i++;
             } while (i < length);
         }
 
-        System.out.print("Hasil Pengurutan = ");
-        asc(array);
-        dsc(array);
+        System.out.print("Sorted Result = ");
+        ascending(array);
+        descending(array);
 
     }
 
-    static void asc(int[] array) {
+    static void ascending(int[] array) {
         for (int j = 0; j < array.length / 2; j++) {
             int min = j;
             for (int k = j; k < array.length / 2; k++) {
-                if (array[min] < array[k]) {
+                if (array[min] > array[k]) {
                     min = k;
                 }
             }
 
-            int temporarry = array[j];
+            int temp = array[j];
             array[j] = array[min];
-            array[min] = temporarry;
-
+            array[min] = temp;
         }
         for (int x = 0; x < array.length / 2; x++) {
             System.out.print(array[x] + " ");
         }
     }
 
-    static void dsc(int[] array) {
+    static void descending(int[] array) {
         for (int j = array.length / 2; j < array.length; j++) {
-            int min = j;
+            int max = j;
             for (int k = j; k < array.length; k++) {
-                if (array[min] > array[k]) {
-                    min = k;
+                if (array[max] < array[k]) {
+                    max = k;
                 }
             }
 
-            int temporarry = array[j];
-            array[j] = array[min];
-            array[min] = temporarry;
-
+            int temp = array[j];
+            array[j] = array[max];
+            array[max] = temp;
         }
         for (int y = array.length / 2; y < array.length; y++) {
             System.out.print(array[y] + " ");
