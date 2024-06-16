c7b7272f2470f1e160f543375e7a56e75d9a8932
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 14:56:34 +0800

    Update Soal4.java

--- praktikum1/Soal4.java
+++ praktikum1/Soal4.java
@@ -1,44 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package praktikum1;
+package week1;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal4 {
-
+public class Case4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int angka;
-        System.out.println("Selamat Datang Pada Program Tabel Perkalian");
-        System.out.print("Perkalian = ");
-        angka = input.nextInt();
+        int number;
+        
+        System.out.println("Welcome to the Multiplication Table Program");
+        
+        System.out.print("Enter a Number = ");
+        number = input.nextInt();
 
         System.out.println("");
         System.out.print("X\t|\t");
 
-        for (int i = 1; i <= angka; i++) {
+        for (int i = 1; i <= number; i++) {
             System.out.print(i + "\t");
         }
 
         System.out.println(" \n ");
         System.out.println("--------------------------------------------------");
 
-        for (int i = 1; i <= angka; i++) {
+        for (int i = 1; i <= number; i++) {
             System.out.print(i + "\t|\t");
-            for (int j = 1; j <= angka; j++) {
+            for (int j = 1; j <= number; j++) {
                 System.out.print(i * j + "\t");
             }
 
             System.out.println("");
-
         }
     }
-
 }
