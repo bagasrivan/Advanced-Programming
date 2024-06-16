5b3c6b90e82a3109b92178ac2fa85bfa4cb957f1
Bagaskara <114666776+bagasrivan@users.noreply.github.com>
2024-06-16 17:35:05 +0800

    Update Soal1.java

--- praktikum6/Soal1.java
+++ praktikum6/Soal1.java
@@ -1,56 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package praktikum6;
+package week6;
 
 import java.util.*;
 
-/**
- *
- * @author Bagaskara
- */
-public class Soal1 {
-
+public class Case1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int bakol, kanan = 0, kiri = 0;
-        System.out.println("Selamat Datang");
-        System.out.print("Masukan Jumlah Matriks = ");
-        bakol = input.nextInt();
+        int size, rightDiagonalSum = 0, leftDiagonalSum = 0;
+        
+        System.out.println("Welcome");
+        
+        System.out.print("Enter the Matrix Size = ");
+        size = input.nextInt();
 
-        int[][] matriks = new int[bakol][bakol];
+        int[][] matrix = new int[size][size];
 
-        for (int i = 0; i < bakol; i++) {
-            for (int j = 0; j < bakol; j++) {
-                int k = i + 1;
-                int l = j + 1;
-                System.out.print("Baris " + k + " Kolom " + l + " = ");
-                matriks[i][j] = input.nextInt();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int row = i + 1;
+                int column = j + 1;
+                System.out.print("Row " + row + " Column " + column + " = ");
+                matrix[i][j] = input.nextInt();
             }
             System.out.println(" ");
         }
-        System.out.println("Bentuk Matriks");
-        for (int i = 0; i < bakol; i++) {
-            for (int j = 0; j < bakol; j++) {
-                System.out.print(matriks[i][j] + " ");
+        System.out.println("Matrix Form");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println(" ");
         }
-        System.out.print("Diagonal Ke Kanan = ");
-        for (int i = 0; i < bakol; i++) {
-            System.out.print(matriks[i][i] + " ");
-            kanan += matriks[i][i];
+        System.out.print("Right Diagonal = ");
+        for (int i = 0; i < size; i++) {
+            System.out.print(matrix[i][i] + " ");
+            rightDiagonalSum += matrix[i][i];
         }
         System.out.println(" ");
-        System.out.print("Diagonal Ke Kiri = ");
-        for (int j = bakol; j > 0; j--) {
-            System.out.print(matriks[bakol - j][j - 1] + " ");
-            kiri += matriks[bakol - j][j - 1];
+        System.out.print("Left Diagonal = ");
+        for (int j = size; j > 0; j--) {
+            System.out.print(matrix[size - j][j - 1] + " ");
+            leftDiagonalSum += matrix[size - j][j - 1];
         }
         System.out.println("");
-        System.out.println("Hasil Penjumlahan Diagonal Ke Kanan = " + kanan);
-        System.out.println("Hasil Penjumlahan Diagonal Ke Kiri = " + kiri);
-        System.out.println("Total = " + (kanan + kiri));
+        System.out.println("Sum of Right Diagonal = " + rightDiagonalSum);
+        System.out.println("Sum of Left Diagonal = " + leftDiagonalSum);
+        System.out.println("Total = " + (rightDiagonalSum + leftDiagonalSum));
     }
 }
